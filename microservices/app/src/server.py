0996c7a9b5c3cc9b3fce18a5c24c4a6ac3c3fa30
bmk15897 <bmk15897@gmail.com>
2018-01-26 13:43:54 +0530

    Sample code

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,101 +1,66 @@
 from flask import Flask,render_template,request,redirect,send_file
 import requests,json
+client = wit.Wit('NLDSCMV6XWVD7K7PID3EXWXIHNWCNRZQ')
 
-ListOfNames=[]  #for storing subtask 7 logs.
+Client_msgs = []
+Wit_Responses = []
 
-app = Flask(__name__)
-
-#Subtask 1 - A simple hello-world at http://localhost:8080/ that displays a simple string like "Hello World - Arpit"
-
-@app.route("/")
-def hello():
-    return "Hello World - Bharati"
-
-
-#Subtask 2 - Add a route, for e.g. http://localhost:8080/authors, which:
-#a) fetches a list of authors from a request to https://jsonplaceholder.typicode.com/users
-#b) fetches a list of posts from a request to https://jsonplaceholder.typicode.com/posts
-#c) Respond with only a list of authors and the count of their posts (a newline for each author).
-
-@app.route("/authors")
-def tp():
-    r = json.loads(requests.get('https://jsonplaceholder.typicode.com/users').content.decode('utf-8'))
-
-    s = json.loads(requests.get('https://jsonplaceholder.typicode.com/posts').content.decode('utf-8'))
-
-    list = {}
-
-    ans = ""
-    for t in s:
-        if t['userId'] in list:
-            list[t['userId']]+=1
-        else:
-            list[t['userId']]=1
-
-    for u in r:
-        ans+=u['name']+"   "+str(list[u['id']])+'<br>'
-    return ans
-
-
-#Subtask 3 - Set a simple cookie (if it has not already been set) at http://localhost:8080/setcookie with the following values: name=<your-first-name> and age=<your-age>.
-
-@app.route("/setcookie",methods=['GET','POST'])
-def setcookie():
-    resp = app.make_response(redirect('/'))
-
-    if 'Username' not in request.cookies:
-        resp.set_cookie('Username','Bharati')
-        print("Username Cookie set")
-    if 'Age' not in request.cookies:
-        resp.set_cookie('Age','20')
-        print("Age Cookie set")
-
-    return resp
-
-
-#Subtask 4 - Fetch the set cookie with http://localhost:8080/getcookies and display the stored key-values in it.
-
-@app.route("/getcookies",methods=['GET','POST'])
-def getcookie():
-    users_name = ""
-    users_age = ""
-    if 'Username' in request.cookies:
-        users_name = request.cookies.get('Username')
-    if 'Age' in request.cookies:
-        users_age = request.cookies.get('Age')
-
-    return users_name+"\t"+users_age
-
-
-#Subtask 5 - Deny requests to your http://localhost:8080/robots.txt page. (or you can use the response at http://httpbin.org/deny if needed)
-
-@app.route("/robots.txt",methods=['GET','POST'])
-def deny_req():
-    return redirect('http://httpbin.org/deny')
-
-
-#Subtask 6 - Render an HTML page at http://localhost:8080/html or an image at http://localhost:8080/image.
-@app.route("/html")
-def user_html():
-    return render_template('sam.html')
-
-@app.route("/image")
-def user_image():
-    return send_file('tiger.png')
-
-
-#Subtask 7 - A text box at http://localhost:8080/input which sends the data as POST to any endpoint of your choice. This endpoint should log the received the received to stdout.
-@app.route("/input")
-def fInput():
-    return render_template('sam.html')
-
-@app.route("/input",methods=['POST','GET'])
+cnt = 0
+@app.route("/",methods=['POST','GET'])
 def fProcess():
-    ListOfNames.append(request.form['text'])
-    print('Logged Info ',ListOfNames)
-    return render_template('sam.html',my_list=ListOfNames)
+    Client_msgs.append(request.form['text'])
+    client_query = Client_msgs[cnt]
+    cnt+=1
+
+    received_response = client.message(client_query)
+    Wit_Responses[cnt-1] = received_response
+
+    #print(received_response['entities'])
+    if 'wit_test' in received_response['entities']:
+        if 'subject' in received_response['entities']:
+            if 'query_all' in received_response['entities']:
+                # show all the tests for that subject
+                temp = str(received_response['entities']['subject'][0]['value'])+str(received_response['entities']['query_all'][0]['value'])
+                return render_template('sam.html',my_list=temp)
+                print('yeah '+str(received_response['entities']['subject'][0]['value'])+str(received_response['entities']['query_all'][0]['value']))
+            else:
+                # show the next test for that subject
+                print('yeah '+str(received_response['entities']['subject'][0]['value'])+str(received_response['entities']['query_next'][0]['value']))
+
+        else:
+            if 'query_all' in received_response['entities']:
+                # show all tests
+                print('bleh')
+            else:
+                # show the next test overall
+                print('code')
+    elif 'wit_homework_read' in received_response['entities']:
+        if 'subject' in received_response['entities']:
+            if 'query_all' in received_response['entities']:
+                # show all the homework for that subject
+                print('code')
+            else:
+                # show the next homework for that subject
+                print('code')
+        else:
+            if 'query_all' in received_response['entities']:
+                # show all homework
+                print('code')
+            else:
+                # show the next homework overall
+                print('code')
+    elif 'wit_timetable_read' in received_response['entities']:
+        # show the timetable
+        print('code')
+    else:
+        # some code
+        print(received_response)
+        # for i in received_response['entities']:
+        #   print(i[0]['value'])
+
+# print(st['msg_id'])
+#client.interactive()
 
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
-
+    # print('Logged Info ',Responses)
+    # return render_template('sam.html',my_list=Responses)
